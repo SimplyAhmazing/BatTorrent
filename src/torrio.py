0cccaf9e95627bf80ae1d329ae00e366ea4d508d
simplyahmazing <ahmad19526@gmail.com>
2017-09-23 22:38:44 -0400

    refactors data files; save downloaded file

--- src/torrio.py
+++ src/torrio.py
@@ -12,7 +12,6 @@ import socket
 import string
 import struct
 from urllib import parse as urlparse
-import warnings
 
 import yarl
 
@@ -23,10 +22,7 @@ logging.basicConfig(
 )
 LOG = logging.getLogger('')
 
-PEER_ID = 'SA' + ''.join(
-    random.choice(string.ascii_lowercase + string.digits)
-    for i in range(18)
-) # 'SimplyAhmazingPython'.encode('utf-8')
+PEER_ID = 'SimplyAhmazingPython'
 PEER_ID_HASH = hashlib.sha1(PEER_ID.encode()).digest()
 CHUNK_SIZE = 10 * 1024
 
@@ -101,12 +97,17 @@ class Tracker(object):
 
     def parse_peers(self, peers : bytes):
         self_addr = socket.gethostbyname(socket.gethostname())
+        self_addr = '192.168.99.1'
+        LOG.info('Self addr is: {}'.format(self_addr))
         def handle_bytes(peers_data):
             peers = []
             for i in range(0, len(peers_data), 6):
-                addr_bytes, port_bytes = peers_data[i:i + 4], peers_data[i + 4:i + 6]
+                addr_bytes, port_bytes = (
+                    peers_data[i:i + 4], peers_data[i + 4:i + 6]
+                )
                 ip_addr = str(ipaddress.IPv4Address(addr_bytes))
                 if ip_addr == self_addr:
+                    print('skipping', ip_addr)
                     continue
                 port_bytes = struct.unpack('>H', port_bytes)[0]
                 peers.append((ip_addr, port_bytes))
@@ -145,6 +146,16 @@ class Peer(object):
             PEER_ID.encode()
         )
 
+    async def send_interested(self, writer):
+        msg = struct.pack('>Ib', 1, 2)
+        writer.write(msg)
+        await writer.drain()
+
+    async def request_a_piece(self, writer):
+        msg = struct.pack('>IbIII', 13, 6, 0, 0, 35)
+        writer.write(msg)
+        await writer.drain()
+
     async def download(self):
         reader, writer = await asyncio.wait_for(
             asyncio.open_connection(self.host, self.port),
@@ -155,7 +166,10 @@ class Peer(object):
         writer.write(self.handshake())
         await writer.drain()
 
+        # TODO: Validate handshake
         handshake = await reader.read(68)  # Suspends here if there's nothing to be read
+
+        await self.send_interested(writer)
 
         buf = b''
         while True:
@@ -165,6 +179,9 @@ class Peer(object):
             buf += resp
 
             LOG.info('Buffer len({}) is {}'.format(len(buf), buf))
+
+            if not buf and not resp:
+                return
 
             while True:
                 if len(buf) < 4:
@@ -184,14 +201,34 @@ class Peer(object):
 
                 msg_id = buf[4] # 5th byte is the ID
 
-                if msg_id == 1:
+                if msg_id == 0:
                     buf = buf[5:]
                     LOG.info('got CHOKE')
+                    continue
+                elif msg_id == 1:
+                    buf = buf[5:]
+                    LOG.info('got UNCHOKE')
+                    await self.request_a_piece(writer)
                     continue
                 elif msg_id == 5:
                     bitfield = buf[5: 5 + length - 1]
                     LOG.info('got bitfield {}'.format(bitfield))
                     buf = buf[5+length-1:]
+
+                    await self.send_interested(writer)
+                    continue
+
+                elif msg_id == 7:
+                    piece_index = buf[5]
+                    piece_begin = buf[6]
+                    block = buf[13: 13 + length]
+                    buf = buf[13 + length:]
+                    LOG.info('Buffer is reduced to {}'.format(buf))
+                    LOG.info('Got piece idx {} begin {}'.format(piece_index, piece_begin))
+                    LOG.info('Got this piece: {}'.format(block))
+
+                    with open(self.torrent.info[b'info'][b'name'].decode(), 'wb') as f:
+                        f.write(block)
                     continue
                 else:
                     LOG.info('unknown ID {}'.format(msg_id))
